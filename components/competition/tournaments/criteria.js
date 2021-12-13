108660bed04d72a7260070d1795edc0fc2d53a9c
Niborwood <robin.souriau@gmail.com>
2021-12-13 19:02:50 +0100

    Added Criteria for Tournaments page

---
+++ components/competition/tournaments/criteria.js
@@ -0,0 +1,142 @@
+import Image from 'next/image';
+
+// MUI IMPORTS
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+function Criteria() {
+  return (
+    <Stack
+          direction={{ xs: 'column', md: 'row' }}
+          justifyContent="space-evenly"
+          alignItems="flex-start"
+          spacing={2}
+        >
+          <Card raised sx={{width: {xs: '100%', md:'30%'}}}>
+            <CardMedia>
+              <Box sx={{ position: 'relative', width: '100%', height: '140px' }}>
+                <Image src="/images/pages/competition/competitions-tournois/regles-generales.jpg" layout="fill" objectFit="cover" alt="Règles générales" />
+              </Box>
+            </CardMedia>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Règles générales
+              </Typography>
+              <Divider sx={{ mb: 2 }} />
+
+              <Typography variant="h6">
+                Règles
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                Le tournoi doit suivre la réglementation en vigueur
+              </Typography>
+              <Typography variant="h6">
+                Communication
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                L&apos;organisateur doit communiquer auprès de la fédération sur les dates du tournoi au moins 6 semaines avant
+              </Typography>
+              <Typography variant="h6">
+                Inscriptions
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                L&apos;ouverture des inscriptions au public doit se faire au minimum 3 semaines avant la date du tournoi
+              </Typography>
+              <Typography variant="h6">
+                Mixité régionale
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                Au moins 3 régions françaises différentes doivent être représentées dans les participants au tournoi
+              </Typography>
+
+            </CardContent>
+          </Card>
+
+          <Card raised sx={{width: {xs: '100%', md:'30%'}}}>
+            <CardMedia>
+              <Box sx={{ position: 'relative', width: '100%', height: '140px' }}>
+                <Image src="/images/pages/competition/competitions-tournois/deroule-tournoi.jpg" layout="fill" objectFit="cover" alt="Règles générales" />
+              </Box>
+            </CardMedia>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Déroulé du tournoi
+              </Typography>
+              <Divider sx={{ mb: 2 }} />
+
+              <Typography variant="h6">
+                Classement
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                L&apos;organisateur doit communiquer à la fédération un classement intégral au plus deux semaines après le tournoi
+              </Typography>
+              <Typography variant="h6">
+                Arbitrage
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                Un observateur au moins doit être présent pour la finale, et si possible pour tous les matchs avez NHZ
+              </Typography>
+              <Typography variant="h6">
+                No-Hit-Zone
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                1-16 équipes : dès la finale ou avant
+                <br />17-32 équipes : dès les 1/2 ou avant
+                <br />33-64 équipes : dès les 1/4 ou avant
+              </Typography>
+              <Typography variant="h6">
+                Seeding
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                Le déroulement du tournoi doit suivre le seeding donné par le classement national à J-5 avant le tournoi
+              </Typography>
+
+            </CardContent>
+          </Card>
+
+          <Card raised sx={{width: {xs: '100%', md:'30%'}}}>
+          <CardMedia>
+              <Box sx={{ position: 'relative', width: '100%', height: '140px' }}>
+                <Image src="/images/pages/competition/competitions-tournois/validation.jpg" layout="fill" objectFit="cover" alt="Règles générales" />
+              </Box>
+            </CardMedia>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Validation
+              </Typography>
+              <Divider sx={{ mb: 2 }} />
+
+              <Typography variant="h6">
+                Poules et bracket
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                Le tournoi doit être réalisé en deux phases : une phase de poules suivie d&apos;une phase à élimination directe
+              </Typography>
+              <Typography variant="h6">
+                Droit de rétractation
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                La fédération se réserve le droit de retirer un tournoi de la liste des tournois officiels, et ce jusqu&apos;à 2 semaines après la date du tournoi
+                <br />
+                <br />
+                Ce retrait ne pourra être effectué que si le tournoi ne répond pas aux critères énoncés par la fédération
+              </Typography>
+              <Typography variant="h6">
+                Confirmation
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+                La fédération s&apos;engage à confirmer les demandes de tournois officiels dans les 2 semaines suivant la demande
+              </Typography>
+
+            </CardContent>
+          </Card>
+        </Stack>
+  )
+}
+
+export default Criteria
--- pages/competition/competitions-et-tournois.js
+++ pages/competition/competitions-et-tournois.js
@@ -1,10 +1,60 @@
+import { Fragment } from 'react';
+
+// MUI IMPORTS
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+
+// COMPONENTS IMPORTS
+import Hero from '../../components/ui/hero';
+import Criteria from '../../components/competition/tournaments/criteria';
 
 function Tournaments() {
   return (
-    <Container >
-      
-    </Container >
+    <Fragment>
+      <Hero
+        title="Compétitions et tournois"
+        image="/images/hero/competitions-tournois.jpg"
+        imagePosition='center center'
+        mini
+      />
+
+      <Container maxWidth="md" sx={{ my: 4 }}>
+
+        <Box sx={{ pb: 4 }}>
+          <Typography variant="h4">Le planning de tous les tournois officiels français</Typography>
+          <Divider sx={{ pb: 2 }} />
+        </Box>
+
+        <Box sx={{ pb: 4 }}>
+          <Typography variant="h5">Les avantages</Typography>
+          <Typography variant="body1">
+            En tant que fédération sportive nationale, Roundnet France organise et accompagne les tournois officiels français de roundnet par l&apos;ensemble des associations et des joueurs, partout en France. En tant que tournoi officiel, vous bénéficiez de nombreux avantages. D&apos;une part, une <strong>visiblité</strong> sur votre tournoi : la fédération communique sur l&apos;événement sur l&apos;ensemble des réseaux sociaux et ses plateformes de communication. D&apos;autre part, un <strong>accompagnement</strong> : une question ? Pas assez de sets ? La fédération est là pour vous accompagner.
+          </Typography>
+        </Box>
+
+        <Box sx={{ pb: 4 }}>
+          <Typography variant="h5">Les critères</Typography>
+          <Typography variant="body1">
+            Les tournois indépendants peuvent compter pour le classement national s&apos;ils respectent les critères suivants.
+          </Typography>
+        </Box>
+
+      </Container >
+
+      <Container maxWidth="lg" sx={{ mb: 4, pb: 4 }}>
+        <Criteria />
+      </Container>
+
+      <Container maxWidth="md" sx={{ my: 4 }}>
+      <Typography variant="h5">Calendrier des tournois à venir</Typography>
+          <Typography variant="body1">
+            Les tournois indépendants peuvent compter pour le classement national s&apos;ils respectent les critères suivants.
+          </Typography>
+      </Container>
+
+    </Fragment>
   )
 }
 
