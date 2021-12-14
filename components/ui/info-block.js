4c38ea3ca7c7c7679b44792f62c1833f2f82cef4
Niborwood <robin.souriau@gmail.com>
2021-12-14 20:53:28 +0100

    Tweaked InfoBlock to be more polyvalent, added Crossing Items component and finished Hall of Fame page

--- components/ui/info-block.js
+++ components/ui/info-block.js
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import propTypes from 'prop-types';
+import { Fragment } from 'react';
 
 // MUI IMPORTS
 import Stack from '@mui/material/Stack'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Paper from '@mui/material/Paper'
+import Chip from '@mui/material/Chip'
 
-function InfoBlock({ title, items, image, description, imageToLeft, height }) {
+function InfoBlock({ title, chip, items, image, description, imageToLeft, height, roundedImage }) {
   // Put the image to left or right on desktops
   const rowOrder = imageToLeft ? 'row-reverse' : 'row';
   // Getting the height of the image
@@ -15,15 +17,34 @@ function InfoBlock({ title, items, image, description, imageToLeft, height }) {
 
   return (
     <Stack
-      direction={{ xs: 'column', md: rowOrder }}
+      direction={{ xs: 'column-reverse', md: rowOrder }}
       justifyContent="space-between"
       alignItems="center"
       spacing={{ xs: 2, md: 4 }}
       my={4}
     >
       <Stack width={{ xs: '100%', md: '50%' }}>
-        <Typography variant="h6" mb={2} color="secondary.main">{title}</Typography>
-        <Typography variant="body2" mb={4}>
+        <Typography
+          variant="h6"
+          mb={2}
+          color="secondary.main"
+          sx={{ textAlign: { xs: 'center', md: imageToLeft ? 'left' : 'right' } }}
+        >
+          {title}
+          {chip && (
+            <Fragment>
+              <br />
+              <Chip color="default" label={chip} />
+            </Fragment>
+          )
+          }
+
+        </Typography>
+        <Typography
+          variant="body2"
+          mb={4}
+          sx={{ textAlign: { xs: 'center', md: imageToLeft ? 'left' : 'right' } }}
+        >
           {description}
         </Typography>
 
@@ -37,7 +58,12 @@ function InfoBlock({ title, items, image, description, imageToLeft, height }) {
               {
                 items.map((item) => (
                   <Box width="50%" key={item.id}>
-                    <Typography variant="h6" color="secondary.main"> {item.title} </Typography>
+                    <Typography
+                      variant="h6"
+                      color="secondary.main"
+                    >
+                      {item.title}
+                    </Typography>
                     <Typography variant="body2">
                       {item.text}
                     </Typography>
@@ -53,7 +79,7 @@ function InfoBlock({ title, items, image, description, imageToLeft, height }) {
 
       </Stack>
 
-      <Paper elevation={3} sx={{ width: { xs: '100%', md: '50%' }, height: {xs: height/2, md: height }, position: 'relative', overflow: 'hidden', borderRadius: 2 }}>
+      <Paper elevation={3} sx={{ width: { xs: '100%', md: '50%' }, height: { xs: height / 1.25, md: height }, position: 'relative', overflow: 'hidden', borderRadius: { xs: 2, md: roundedImage ? 60 : 2 } }}>
         <Image
           src={image}
           layout='fill'
@@ -73,12 +99,16 @@ InfoBlock.propTypes = {
   description: propTypes.string.isRequired,
   imageToLeft: propTypes.bool,
   height: propTypes.number,
+  roundedImage: propTypes.bool,
+  chip: propTypes.string,
 }
 
 InfoBlock.defaultProps = {
   items: null,
   imageToLeft: false,
   height: 400,
+  roundedImage: false,
+  chip: null,
 }
 
 export default InfoBlock
