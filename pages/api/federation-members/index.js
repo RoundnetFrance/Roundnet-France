c2b6a132761d7306c163aebbd3e6580ff89d7ba8
Niborwood <robin.souriau@gmail.com>
2021-12-16 14:24:10 +0100

    Added MongoDB config, first getStatic on Federation Team Page and basic Error Component

---
+++ pages/api/federation-members/index.js
@@ -0,0 +1,21 @@
+// NEXT API REQUEST
+// GET /api/v1/federation-members
+
+const { connectToDatabase } = require('../../../lib/mongodb');
+
+export default async function handler(req, res) {
+  // GET method to read federation members
+  if (req.method === 'GET') {
+    try {
+      const { db } = await connectToDatabase();
+      const federationMembers = await db.collection('federation-members').find({}).toArray();
+      return res.status(200).json(federationMembers);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+  return res.status(405).json({ error: 'Method not allowed' });
+
+
+}
