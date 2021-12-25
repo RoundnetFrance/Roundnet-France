7b0ee0b1acdc13c28e07869c83d96f8a0ebc98a9
Niborwood <robin.souriau@gmail.com>
2021-12-25 17:17:20 +0100

    Added the matchFormWithApiSchema function in helper/db with error handling

--- pages/api/clubs/index.js
+++ pages/api/clubs/index.js
@@ -22,68 +22,24 @@ export default async function handler(req, res) {
 
     // * Validate the data
     // Validate when submitted directly through API POST request
-    if (!formBuilder) {
-      try {
-        console.log('Create a specific function to validate from API (with schema, without fields)');
-      }
-      catch (error) {
-        // Return an error message concerning invalid data values
-        return res.status(400).json({ error: error.message || 'Invalid data' });
-      }
+    try {
+      console.log('validate');
+    } catch (error) {
+      return res.status(400).json({ error: 'Erreur de validation des données', details: error.message });
     }
-    // Validate when the form is submitted from the client
-    else {
-      try {
-        // Add links if any
-        const links = [];
-        if (data.facebook) {
-          links.push({
-            name: 'facebook',
-            url: data.facebook,
-          });
-        }
-        if (data.instagram) {
-          links.push({
-            name: 'instagram',
-            url: data.instagram,
-          });
-        }
-        if (data.website) {
-          links.push({
-            name: 'website',
-            url: data.website,
-          });
-        }
-      } catch (error) {
-        console.log(error)
-      }
+
+    // * Send the data to the database
+    try {
+      // const response = await insertDocument('clubs', newClub);
+      // console.log(response);
+
+      return res.status(201).json({ message: 'Le club a bien été enregistré. Il sera validé par un membre de la fédération.' });
+
+    } catch (error) {
+      return res.status(500).json({ error: 'Une erreur est survenue lors de la création du club dans la base de données. Veuillez réessayer.', details: error.message });
     }
   }
 
-  // Send the data to the database
-  try {
 
-
-    // Creating the club data object
-    const newClub = {
-      title: data.organization || data.title,
-      chip: data.city || data.chip,
-      description: data.description,
-      links: links.length > 0 ? links : data.links,
-      president: data.name || data.president,
-      validated: false,
-    }
-
-    const response = await insertDocument('clubs', newClub);
-    console.log(response);
-
-    // return res.status(201).json({ message: 'Le club a bien été enregistré. Il sera validé par un membre de la fédération.' });
-
-  } catch (error) {
-    return res.status(500).json({ error: 'Une erreur est survenue lors de la création du club dans la base de données. Veuillez réessayer.', details: error.message });
-  }
-}
-
-
-return res.status(405).json({ error: 'Method not allowed' });
+  return res.status(405).json({ error: 'Method not allowed' });
 }
