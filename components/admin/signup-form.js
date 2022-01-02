576a5113c1364c80f77ded2110fb1611b0f8ed13
Niborwood <robin.souriau@gmail.com>
2022-01-02 13:13:29 +0100

    Transformed signUp form with FormBuilder. Added password and passwordCnfirm handling for FormBuilder.

--- components/admin/signup-form.js
+++ components/admin/signup-form.js
@@ -1,126 +1,65 @@
-import { useState } from 'react';
-import handleFormSubmit from '../../helpers/handle-form-submit';
+import { Fragment } from 'react';
 
 // MUI IMPORTS
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import CircularProgress from '@mui/material/CircularProgress';
-import Slide from '@mui/material/Slide';
-import Alert from '@mui/material/Alert';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Snackbar from '@mui/material/Snackbar';
-import Divider from '@mui/material/Divider';
+import { Typography } from '@mui/material'
 
 // COMPONENT IMPORT
+import FormBuilder from '../form-builder';
 import Link from '../ui/link';
-import BoxWrapper from '../ui/box-wrapper';
-import PasswordInput from '../ui/password-input';
 
 // FUNCTIONAL COMPONENT
 function SignUpForm() {
-
-  // Handle controlled inputs
-  const initialFormState = {
-    name: '',
-    email: '',
-    password: '',
-    passwordConfirm: '',
-  }
-  const [form, setForm] = useState(initialFormState);
-  const handleChange = (event) => {
-    const { id, value } = event.target;
-    setForm((prevForm) => ({
-      ...prevForm,
-      [id]: value,
-    }));
-  };
-
-  // Handle errors
-  const [errors, setErrors] = useState(initialFormState);
-  // Handle loading
-  const [loading, setLoading] = useState(false);
-
-  // Handle submit status
-  const [submitStatus, setSubmitStatus] = useState({
-    open: false,
-    success: false,
-    message: '',
-    error: false,
-  });
-
-  // Handle submission (through handleFormSubmit helper function)
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirm'];
-    handleFormSubmit(
-      setLoading,
-      setErrors,
-      setForm,
-      setSubmitStatus,
-      form,
-      errors,
-      '/api/auth/signup',
-      requiredFields,
-    );
-  };
-
-  // Handle close of snackbar
-  const handleSnackbarClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    // Reset submit status
-    setSubmitStatus((prevSubmitStatus) => ({
-      ...prevSubmitStatus,
-      open: false,
-    }));
-  };
-
-  return (
-
-    <BoxWrapper title="Créer un compte" onSubmit={handleSubmit}>
+  const descriptionBefore = (
+    <Fragment>
       <Typography>La validation d&apos;un compte administrateur Roundnet France permet aux associations d&apos;enregistrer les résultats des tournois automatiquement. Elle est soumise à l&apos;acceptation d&apos;un membre du board administratif de la fédération. </Typography>
       <Typography>Pour en savoir plus, <Link href="/qui-sommes-nous/contact">contactez-nous directement.</Link></Typography>
+    </Fragment>);
 
-      <Divider />
+    const formConfig = {
+      name: 'Créer un compte',
+      fields: [
+        {
+          id: 'name',
+          label: 'Nom & Prénom',
+          type: 'text',
+          options: {
+            required: true,
+          }
+        },
+        {
+          id: 'email',
+          label: 'Email',
+          type: 'email',
+          options: {
+            required: true,
+          }
+        },
+        {
+          id: 'password',
+          label: 'Mot de passe',
+          type: 'password',
+          options: {
+            required: true,
+          }
+        },
+        {
+          id: 'passwordConfirm',
+          label: 'Confirmation du mot de passe',
+          type: 'password',
+          options: {
+            required: true,
+            passwordConfirm: true,
+          }
+        },
+      ],
+      endpoint: '/auth/signup',
+      submitText: 'Créer un compte',
+      descriptionBefore,
+    };
 
-      <TextField id="name" label="Nom & Prénom" variant="outlined" value={form.name} onChange={handleChange} error={errors.name !== ""} helperText={errors.name} required />
-      <TextField id="email" label="Email" variant="outlined" value={form.email} onChange={handleChange} error={errors.email !== ""} helperText={errors.email} required />
 
-      <PasswordInput label="Mot de passe" value={form.password} name="password" handleChange={handleChange} error={errors.password !== ''} helperText={errors.password} />
-      <PasswordInput label="Confirmation du mot de passe" value={form.passwordConfirm} name="passwordConfirm" handleChange={handleChange} error={errors.passwordConfirm !== ''} helperText={errors.passwordConfirm} confirm />
-      
-      <Typography variant="body2" >
-        * Champs obligatoires
-      </Typography>
-      <Button variant="contained" color="primary" type="submit">
-        {loading ? (<Box sx={{ display: 'flex' }}>
-          <CircularProgress color="inherit" size={30} />
-        </Box>) : 'Envoyer'}
-      </Button>
-      <Typography variant="body2" >
-        Vous avez déjà un compte ? <Link href="/rf-admin">Connectez-vous</Link>
-      </Typography>
-
-      {/* SNACKBAR */}
-      <Snackbar
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }
-        }
-        open={submitStatus.open}
-        autoHideDuration={5000}
-        onClose={handleSnackbarClose}
-        TransitionComponent={Slide}
-      >
-        <Alert
-          onClose={handleSnackbarClose}
-          severity={submitStatus.success ? 'success' : 'error'} sx={{ width: '100%' }}>
-          {submitStatus.message}
-        </Alert>
-      </Snackbar>
-    </BoxWrapper>
-
+  return (
+    <FormBuilder formConfig={formConfig} />  
   )
 }
 
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,59 +1,86 @@
-import { getUser, insertUser } from "../../../helpers/db/users";
+import { getDocument, insertDocument } from "../../../helpers/db";
 import { hash } from 'bcryptjs';
-import validateEmail from "../../../helpers/validate-email";
+import { validateAPI } from '../../../helpers/form';
+import Joi from 'joi';
+
 
 export default async function handler(req, res) {
     //Only POST mothod is accepted
     if (req.method === 'POST') {
-        //Getting email and password from body
-        const { name, email, password, passwordConfirm } = req.body;
+        // //Getting email and password from body
+        // const { name, email, password, passwordConfirm } = req.body;
 
-        //Validate
-        const emailIsValid = validateEmail(email);
-        if (!email || !emailIsValid) {
-            res.status(422).json({ input: 'email', message: 'L\'email n\'est pas valide' });
-            return;
+        // //Validate
+        // const emailIsValid = validateEmail(email);
+        // if (!email || !emailIsValid) {
+        //     res.status(422).json({ input: 'email', message: 'L\'email n\'est pas valide' });
+        //     return;
+        // }
+
+        // if (!name || name.trim() === '') {
+        //     res.status(422).json({ input: 'name', message: 'Le nom est requis' });
+        //     return;
+        // }
+
+        // if (!password || password.length < 6) {
+        //     res.status(422).json({ input: 'password', message: 'Le mot de passe n\'est pas valide. Il doit faire plus de 6 caractères.' });
+        //     return;
+        // }
+
+        // if (password !== passwordConfirm) {
+        //     res.status(422).json({ input: 'passwordConfirm', message: 'Les mots de passe ne correspondent pas' });
+        // }
+
+        const { data } = req.body;
+
+        // * Validate the data
+        try {
+
+            // Define the POST CLUB schema
+            const schema = Joi.object({
+                name: Joi.string().trim().required(),
+                email: Joi.string().trim().email({ tlds: { allow: false } }).required(),
+                password: Joi.string().min(6).required(),
+                passwordConfirm: Joi.any().valid(Joi.ref('password')).required(),
+            });
+
+            // Actual validation
+            validateAPI({ data, schema });
+
+        } catch (error) {
+            console.error('ERROR 400 - users', error.message);
+            return res.status(400).json({ message: error.message });
         }
 
-        if (!name || name.trim() === '') {
-            res.status(422).json({ input: 'name', message: 'Le nom est requis' });
-            return;
+        // * Send the validated data to the database
+        try {
+            //Check existing
+            const userExists = await getDocument('users', { email: data.email });
+            //Send error response if duplicate user is found
+            if (userExists) {
+                res.status(403).json({ message: 'User already exists' });
+                return;
+            }
+
+            // Hash password & remove passwordConfirm
+            data.password = await hash(data.password, 12);
+            delete data.passwordConfirm;
+
+            // Add authorized:false && image:'' properties to the data.
+            data.authorized = false;
+            data.image = '';
+
+            // Insert user
+            await insertDocument('users', data);
+
+            //Send success response
+            res.status(201).json({ message: 'Le compte a bien été créé. Vous serez notifié par email lorsqu\'il sera validé par la fédération.' });
+
+        } catch (error) {
+            console.error('ERROR 500, users : ', error.message);
+            return res.status(500).json({ error: 'Une erreur est survenue lors de la création du club dans la base de données. Veuillez réessayer.', details: error.message });
         }
 
-        if (!password || password.length < 6) {
-            res.status(422).json({ input: 'password', message: 'Le mot de passe n\'est pas valide. Il doit faire plus de 6 caractères.' });
-            return;
-        }
-
-        if (password !== passwordConfirm) {
-            res.status(422).json({ input: 'passwordConfirm', message: 'Les mots de passe ne correspondent pas' });
-        }
-
-
-        //Check existing
-        const userExists = await getUser({ email });
-        //Send error response if duplicate user is found
-        if (userExists) {
-            res.status(403).json({ message: 'User already exists' });
-            return;
-        }
-
-        
-
-
-        //Hash password & insert user
-        await insertUser({
-            name,
-            email,
-            authorized: false,
-            image: '',
-            // createdAt: new Date(),
-            // updatedAt: new Date(),
-            password: await hash(password, 12),
-        });
-
-        //Send success response
-        res.status(201).json({ message: 'Le compte a bien été créé. Vous serez notifié par email lorsqu\'il sera validé par la fédération.' });
     } else {
         //Response for other than POST method
         res.status(405).json({ message: 'Method not allowed' });
