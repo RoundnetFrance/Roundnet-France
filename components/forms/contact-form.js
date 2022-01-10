dcf1bc952295fc68dccf1dad6e5d031eeeec286e
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-10 11:59:23 +0100

    Added default handling for select values on FormBuilder

--- components/forms/contact-form.js
+++ components/forms/contact-form.js
@@ -33,6 +33,7 @@ function ContactForm() {
             {
               value: 'question',
               label: 'Question',
+              default: true,
             },
             {
               value: 'demande',
