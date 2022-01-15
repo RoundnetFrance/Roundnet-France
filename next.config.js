589adeb9da45db3753f5c7705995b1e3b4096902
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-15 10:51:28 +0100

    Changed /competition/regles to /regles with redirect in next config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,20 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
-      'picsum.photos', 
+      'picsum.photos',
       'images.unsplash.com',
       'lh3.googleusercontent.com',
       'platform-lookaside.fbsbx.com',
       'firebasestorage.googleapis.com',
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/competition/regles',
+        destination: '/regles',
+        permanent: true,
+      },
+    ];
+  }
 }
