4f7b8d48432dbc832dde4da66e2cd073e54f42d0
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-19 11:47:25 +0100

    Mentions Légales content inserted

--- contents/misc/index.js
+++ contents/misc/index.js
@@ -1,26 +1,33 @@
 const mentionsLegalesElements = [
   {
-    title: "Article 1",
-    content: ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta et maiores autem sapiente quod similique ullam labore possimus accusamus esse, molestiae nihil dolore iste consequatur nisi aperiam hic nesciunt. Excepturi iusto perferendis tempore ratione sint recusandae, beatae explicabo porro, doloribus nemo doloremque ipsum laudantium blanditiis architecto rerum deleniti tenetur provident neque cumque, odio eveniet fugiat quo! Reiciendis molestias incidunt iure, aliquid illo totam nulla.',
-      '    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta et maiores autem sapiente quod similique ullam labore possimus accusamus esse, molestiae nihil dolore iste consequatur nisi aperiam hic nesciunt. Excepturi iusto perferendis tempore ratione sint recusandae, beatae explicabo porro, doloribus nemo doloremque ipsum laudantium blanditiis architecto rerum deleniti tenetur provident neque cumque, odio eveniet fugiat quo! Reiciendis molestias incidunt iure, aliquid illo totam nulla.',
+    title: "ARTICLE 1 - L'EDITEUR",
+    content: [
+      "L'édition du Site est assurée par la FEDERATION FRANCAISE DE ROUNDNET (FFR) association loi 1901 dont le siège social est situé au 6 rue des Marronniers 75016 Paris.",
+      "Adresse email : roundnetfrance@gmail.com",
+      "Le directeur de la publication est M Adrien Tacquard, ci-après dénommé \"l'Editeur.\"",
     ]
   },
   {
-    title: "Article 2",
-    content: ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta et maiores autem sapiente quod similique ullam labore possimus accusamus esse, molestiae nihil dolore iste consequatur nisi aperiam hic nesciunt. Excepturi iusto perferendis tempore ratione sint recusandae, beatae explicabo porro, doloribus nemo doloremque ipsum laudantium blanditiis architecto rerum deleniti tenetur provident neque cumque, odio eveniet fugiat quo! Reiciendis molestias incidunt iure, aliquid illo totam nulla.',
-      '    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta et maiores autem sapiente quod similique ullam labore possimus accusamus esse, molestiae nihil dolore iste consequatur nisi aperiam hic nesciunt. Excepturi iusto perferendis tempore ratione sint recusandae, beatae explicabo porro, doloribus nemo doloremque ipsum laudantium blanditiis architecto rerum deleniti tenetur provident neque cumque, odio eveniet fugiat quo! Reiciendis molestias incidunt iure, aliquid illo totam nulla.',
+    title: "ARTICLE 2 - L'HEBERGEUR",
+    content: [
+      "L'hébergeur du Site est la société Vercel, dont le siège social est situé au 340 S Lemon Ave #4133 Walnut, CA 91789, avec l'adresse mail de contact : privacy@vercel.com"
     ]
   },
   {
-    title: "Article 3",
-    content: ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta et maiores autem sapiente quod similique ullam labore possimus accusamus esse, molestiae nihil dolore iste consequatur nisi aperiam hic nesciunt. Excepturi iusto perferendis tempore ratione sint recusandae, beatae explicabo porro, doloribus nemo doloremque ipsum laudantium blanditiis architecto rerum deleniti tenetur provident neque cumque, odio eveniet fugiat quo! Reiciendis molestias incidunt iure, aliquid illo totam nulla.',
-      '    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta et maiores autem sapiente quod similique ullam labore possimus accusamus esse, molestiae nihil dolore iste consequatur nisi aperiam hic nesciunt. Excepturi iusto perferendis tempore ratione sint recusandae, beatae explicabo porro, doloribus nemo doloremque ipsum laudantium blanditiis architecto rerum deleniti tenetur provident neque cumque, odio eveniet fugiat quo! Reiciendis molestias incidunt iure, aliquid illo totam nulla.',
+    title: "ARTICLE 3 - ACCES AU SITE",
+    content: [
+      "Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.",
+      "En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable."
     ]
   },
   {
-    title: "Article 4",
-    content: ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta et maiores autem sapiente quod similique ullam labore possimus accusamus esse, molestiae nihil dolore iste consequatur nisi aperiam hic nesciunt. Excepturi iusto perferendis tempore ratione sint recusandae, beatae explicabo porro, doloribus nemo doloremque ipsum laudantium blanditiis architecto rerum deleniti tenetur provident neque cumque, odio eveniet fugiat quo! Reiciendis molestias incidunt iure, aliquid illo totam nulla.',
-      '    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta et maiores autem sapiente quod similique ullam labore possimus accusamus esse, molestiae nihil dolore iste consequatur nisi aperiam hic nesciunt. Excepturi iusto perferendis tempore ratione sint recusandae, beatae explicabo porro, doloribus nemo doloremque ipsum laudantium blanditiis architecto rerum deleniti tenetur provident neque cumque, odio eveniet fugiat quo! Reiciendis molestias incidunt iure, aliquid illo totam nulla.',
+    title: "ARTICLE 4 - COLLECTE DES DONNEES",
+    content: [
+      "Le Site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. ",
+      "En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :",
+      "- via un formulaire de contact ;",
+      "- via son espace personnel, pour les administrateurs du site ;",
+      "Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site, sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil."
     ]
   },
 ];
