30e1a663ce07dbbd48b31d51c7dbd1d7f7d7333f
Niborwood <robin.souriau@gmail.com>
2022-01-24 16:38:39 +0100

    Fixed no date on account-club settings

--- components/layout/admin/dashboard-wrapper.js
+++ components/layout/admin/dashboard-wrapper.js
@@ -1,17 +1,17 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
 // MUI IMPORTS
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 
 function DashboardWrapper({ children }) {
   return (
-    <Box sx={{ backgroundColor: 'primary.lightest', minHeight: '80vh' }}>
-      <Container maxWidth="xl" sx={{ py: 4 }}>
+    <Box sx={{ backgroundColor: "primary.lightest", minHeight: "80vh" }}>
+      <Container maxWidth="lg" sx={{ py: 4 }}>
         {children}
       </Container>
     </Box>
-  )
+  );
 }
 
-export default DashboardWrapper
+export default DashboardWrapper;
--- hooks/useClubs.js
+++ hooks/useClubs.js
@@ -1,13 +1,12 @@
-import fetcher from '../lib/swr-fetcher';
-import useSWR from 'swr';
+import fetcher from "../lib/swr-fetcher";
+import useSWR from "swr";
 
 export default function useClubs() {
-
-  const { data, error } = useSWR('/api/clubs/admin', fetcher)
+  const { data, error } = useSWR("/api/clubs", fetcher);
 
   return {
     clubs: data,
     isLoading: !error && !data,
-    isError: error
-  }
+    isError: error,
+  };
 }
