3b1b1787728d5b709c56d9f4022e14d54b3ed95a
Niborwood <robin.souriau@gmail.com>
2022-01-24 17:12:15 +0100

    Fixed rules page with wrong calendar link

--- components/admin/admin-content/admin-content-single.js
+++ components/admin/admin-content/admin-content-single.js
@@ -1,6 +1,16 @@
-import React from "react";
+// MUI IMPORTS
+import { Container, Paper } from "@mui/material";
+
+import PageTitle from "../../../components/ui/page-title";
 
 export default function AdminContentSingle({ config }) {
   console.log(config);
-  return <div>Hello</div>;
+  return (
+    <Container maxWidth="lg" sx={{ mt: 5, mb: 2 }}>
+      <PageTitle title={config.title} />
+      <Paper>
+        <p>Coucou</p>
+      </Paper>
+    </Container>
+  );
 }
