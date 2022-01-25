ab6da64caff00b79c0b89f441383126a05796665
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-25 09:44:49 +0100

    Added support for array in AdminContent

--- components/admin/admin-content/admin-content-single/data-fields.js
+++ components/admin/admin-content/admin-content-single/data-fields.js
@@ -2,9 +2,9 @@
 import SingleField from "./single-field";
 
 export default function DataFields({ layout, values, handleValuesChange }) {
+  console.log(values);
   // Filter only values that are in layout
   const filteredValues = layout.reduce((acc, field) => {
-    console.log(field._id);
     if (values[field._id] !== null || values[field._id] !== undefined) {
       acc[field._id] = values[field._id];
     }
@@ -13,12 +13,14 @@ export default function DataFields({ layout, values, handleValuesChange }) {
 
   // Return fields corresponding to selected tab
   return Object.keys(filteredValues).map((key) => {
-    const fieldOptions = layout.find((field) => field._id === key);
+    // Get field layout through its key/_id
+    const fieldLayout = layout.find((field) => field._id === key);
+
     return (
       <SingleField
         key={key}
         id={key}
-        fieldOptions={fieldOptions}
+        fieldLayout={fieldLayout}
         value={filteredValues[key]}
         handleValuesChange={handleValuesChange}
       />
--- pages/rf-admin/edit/clubs/[clubId].js
+++ pages/rf-admin/edit/clubs/[clubId].js
@@ -6,99 +6,8 @@ import fetcher from "../../../../lib/swr-fetcher";
 // COMPONENT IMPORTS
 import AdminContentSingle from "../../../../components/admin/admin-content/admin-content-single";
 
-// Data Config
-const config = {
-  title: "Modification de club",
-  tabs: [
-    // GENERAL INFO
-    {
-      _id: "main",
-      name: "Général",
-      description: "Informations générales du club",
-      layout: [
-        {
-          _id: "title",
-          name: "Club",
-          type: "text",
-          editable: true,
-        },
-        {
-          _id: "chip",
-          name: "Ville",
-          type: "text",
-          editable: true,
-        },
-        {
-          _id: "description",
-          name: "Description du club",
-          type: "longtext",
-          editable: true,
-        },
-        {
-          _id: "validated",
-          name: "Validé",
-          align: "right",
-          type: "boolean",
-          editable: true,
-        },
-      ],
-    },
-    // Contact
-    {
-      _id: "contact",
-      name: "Contact",
-      description:
-        "Contact et personnes référentes de l'association ou du club",
-      layout: [
-        {
-          _id: "referer",
-          name: "Référent",
-          type: "text",
-          editable: true,
-        },
-        {
-          _id: "email",
-          name: "Email",
-          type: "text",
-        },
-        {
-          _id: "phone",
-          name: "Téléphone",
-          type: "text",
-        },
-      ],
-    },
-    // Extras
-    {
-      _id: "extras",
-      name: "Extras",
-      description: "Informations additionnelles",
-      layout: [
-        {
-          _id: "players",
-          name: "Joueurs",
-          type: "text",
-          editable: true,
-        },
-        {
-          _id: "links",
-          name: "Liens",
-          editable: true,
-          type: "array",
-          array: {
-            key: "source",
-            value: "url",
-          },
-        },
-        {
-          _id: "discord",
-          name: "Discord",
-          editable: true,
-        },
-      ],
-    },
-  ],
-};
+// CONTENTS
+import clubConfig from "../../../../contents/forms/clubs";
 
 export default function EditAdminPage() {
   // Get endpoint and ID from URL
@@ -112,7 +21,7 @@ export default function EditAdminPage() {
   //! If club data is loading
   if (isLoading) return <p>Loading...</p>;
 
-  return <AdminContentSingle config={config} data={data} />;
+  return <AdminContentSingle config={clubConfig} data={data} />;
 }
 
 // NextJS functions
