30a6355cd6af6f4f46f404b09ad4701e92133545
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-26 19:55:52 +0100

    Added adminUIv2 to admins page

---
+++ pages/rf-admin/edit/users/[userId].js
@@ -0,0 +1,49 @@
+import { useRouter } from "next/router";
+import { getDocuments } from "../../../../helpers/db";
+import useSWR from "swr";
+import fetcher from "../../../../lib/swr-fetcher";
+
+// COMPONENT IMPORTS
+import AdminContentSingle from "../../../../components/admin/admin-content/admin-content-single";
+
+// CONTENTS
+import adminConfig from "../../../../contents/forms/admins";
+
+export default function EditAdminPage() {
+  // Get endpoint and ID from URL
+  const router = useRouter();
+  const { userId } = router.query;
+
+  // Get club data
+  const { data, error, mutate } = useSWR(`/api/users/${userId}`, fetcher);
+  const isLoading = !error && !data;
+
+  return (
+    <AdminContentSingle
+      config={adminConfig}
+      data={data}
+      mutate={mutate}
+      documentId={userId}
+      isLoading={isLoading}
+    />
+  );
+}
+
+// NextJS functions
+export async function getStaticPaths() {
+  const data = await getDocuments("users");
+  const paths = data.map((user) => ({
+    params: {
+      userId: user._id,
+    },
+  }));
+  return { paths, fallback: false };
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      adminLayout: true,
+    },
+  };
+}
