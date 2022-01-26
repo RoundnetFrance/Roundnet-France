1a60cba8e0d3ac1d5075ad58a210b2f2c727577e
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-26 19:05:48 +0100

    Fixed warning on dashboard

--- pages/api/send-mail/index.js
+++ pages/api/send-mail/index.js
@@ -1,12 +1,11 @@
-import sgMail from '@sendgrid/mail'
-import emailTeplate from '../../../styles/email-templates/contact-email-template'
+import sgMail from "@sendgrid/mail";
+import emailTeplate from "../../../styles/email-templates/contact-email-template";
 
 // Sign SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function (req, res) {
-  if (req.method === 'POST') {
-
+  if (req.method === "POST") {
     const { data } = req.body;
 
     // Create email template
@@ -14,8 +13,8 @@ export default async function (req, res) {
 
     // Create email object
     const msg = {
-      to: 'robin.souriau@gmail.com',
-      from: 'robin.souriau@gmail.com',
+      to: "roundnetfrance@gmail.com",
+      from: "roundnetfrance@gmail.com",
       subject: `Contact via le site - ${data.subject}`,
       name: data.name,
       html,
@@ -25,12 +24,17 @@ export default async function (req, res) {
       await sgMail.send(msg);
       //! FOR TESTING PURPOSES ONLY - Wait for 2 seconds
       // await new Promise((resolve) => setTimeout(resolve, 2000));
-      res.status(200).json({ success: true, message: `Votre email a été envoyé ! Nous vous répondrons sous peu.` })
+      res.status(200).json({
+        success: true,
+        message: `Votre email a été envoyé ! Nous vous répondrons sous peu.`,
+      });
     } catch (error) {
-      res.status(500).json({ message: 'Une erreur est survenue lors de l\'envoi du mail. Merci de réessayer' })
+      res.status(500).json({
+        message:
+          "Une erreur est survenue lors de l'envoi du mail. Merci de réessayer",
+      });
     }
   } else {
-    res.status(405).json({ error: 'Method not allowed' })
+    res.status(405).json({ error: "Method not allowed" });
   }
-
 }
