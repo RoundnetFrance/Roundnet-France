92e01da40e44ac850ae0dc86f2aa416fad1b065c
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-27 16:34:44 +0100

    Added Alert message on CreateClub page

--- components/ui/link.js
+++ components/ui/link.js
@@ -1,22 +1,39 @@
-import propTypes from 'prop-types';
+import propTypes from "prop-types";
 import NextLink from "next/link";
 import MUILink from "@mui/material/Link";
 
 export default function Link({ href, children, color, underline }) {
+  // If href begins with http, return only MUI link with _blank target
+  if (href.startsWith("http")) {
+    return (
+      <MUILink
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        color={color}
+        underline={underline}
+      >
+        {children}
+      </MUILink>
+    );
+  }
+
   return (
     <NextLink href={href} passHref>
-      <MUILink color={color} underline={underline}>{children}</MUILink>
+      <MUILink color={color} underline={underline}>
+        {children}
+      </MUILink>
     </NextLink>
-  )
+  );
 }
 
 Link.propTypes = {
   href: propTypes.string.isRequired,
   color: propTypes.string,
   underline: propTypes.string,
-}
+};
 
 Link.defaultProps = {
-  color: 'primary',
-  underline: 'always',
-}
+  color: "primary",
+  underline: "always",
+};
--- pages/clubs-et-communautes/creer-votre-club.js
+++ pages/clubs-et-communautes/creer-votre-club.js
@@ -1,19 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
 // MUI IMPORTS
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Alert, AlertTitle } from "@mui/material";
 
 // COMPONENT IMPORTS
-import Hero from '../../components/ui/hero';
-import PageTitle from '../../components/ui/page-title';
-import Head from '../../components/head';
-import CreateClubForm from '../../components/forms/create-club-form';
-import Link from '../../components/ui/link';
+import Hero from "../../components/ui/hero";
+import PageTitle from "../../components/ui/page-title";
+import Head from "../../components/head";
+import CreateClubForm from "../../components/forms/create-club-form";
+import Link from "../../components/ui/link";
 
 export default function CreateClubPage() {
   return (
     <Fragment>
-
       <Head
         title="Créer votre club - Roundnet France"
         description="Créez votre club et rejoignez la communauté Roundnet France."
@@ -22,18 +21,35 @@ export default function CreateClubPage() {
       <Hero
         title="Créer votre club"
         image="/images/hero/liste-clubs.jpg"
-        imagePosition='center 60%'
-        mini />
+        imagePosition="center 60%"
+        mini
+      />
 
       <Container maxWidth="md" sx={{ my: 4 }}>
         <PageTitle title="Devenez club officiel affilié à Roundnet France" />
-        <Typography variant="body1">
-          Si vous souhaitez plus d&apos;informations sur les avantages et les accompagnements que proposent Roundnet France, vous pouvez <Link href="/clubs-et-communautes/adherer-a-roundnet-france">cliquer sur ce lien pour plus d&apos;informations.   </Link>
+        <Typography variant="body1" gutterBottom>
+          Si vous souhaitez plus d&apos;informations sur les avantages et les
+          accompagnements que proposent Roundnet France, vous pouvez{" "}
+          <Link href="/clubs-et-communautes/adherer-a-roundnet-france">
+            cliquer sur ce lien pour plus d&apos;informations.{" "}
+          </Link>
         </Typography>
+        <Alert severity="info" sx={{ mt: 4 }}>
+          <AlertTitle>
+            <strong>Important !</strong>
+          </AlertTitle>
+          <Typography variant="body1">
+            N&apos;oubliez pas de payer votre cotisation <strong>avant</strong>{" "}
+            de remplir ce formulaire via ce lien :<br />
+            <Link href="https://www.helloasso.com/associations/roundnet-france/adhesions/adhesion-des-clubs-a-la-federation">
+              <strong>Formulaire d&apos;adhésion HelloAsso</strong>
+            </Link>
+            .
+          </Typography>
+        </Alert>
       </Container>
 
       <CreateClubForm />
-
     </Fragment>
-  )
+  );
 }
