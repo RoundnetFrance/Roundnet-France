210ea2be71bcc744e02ae6d2cbce0dee6bb4e0f8
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-27 01:21:40 +0100

    Prepared for partners admin

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 // Reset margins and paddings
-import '../styles/globals.css';
+import "../styles/globals.css";
 
 // Create a layout and theme for all front pages (header + footer)
-import Layout from '../layout'
-import Head from 'next/head'
-import { ThemeProvider } from '@mui/material/styles';
-import rfMuiTheme from '../styles/rf-mui-theme';
+import Layout from "../layout";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import rfMuiTheme from "../styles/rf-mui-theme";
 
 // Import Provider from next/auth client to share session
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider } from "next-auth/react";
 
 // Render app
 function MyApp({ Component, pageProps }) {
@@ -18,13 +18,16 @@ function MyApp({ Component, pageProps }) {
         <Layout adminLayout={pageProps.adminLayout} session={pageProps.session}>
           <Head>
             <title>Roundnet France - Fédération française de roundnet</title>
-            <meta name="description" content="Site officiel de la fédération française de roundnet" />
+            <meta
+              name="description"
+              content="Site officiel de la fédération française de roundnet"
+            />
           </Head>
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
     </SessionProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/api/partners/index.js
+++ pages/api/partners/index.js
@@ -1,23 +1,17 @@
-import { getDocuments, insertDocument } from "../../../helpers/db";
 import { getSession } from "next-auth/react";
+import { getDocuments, insertDocument } from "../../../helpers/db";
 import { validateAPI } from "../../../helpers/form";
 import getSchema from "../../../helpers/schemas";
 
 export default async function handler(req, res) {
-  // Get the doctype. Will be used to only fetch docs of that type
-  const { doctype } = req.query;
-
   const session = await getSession({ req });
-  // GET method to read rule files
+
+  // If partner is authorized
+  // GET method to read app members
   if (req.method === "GET") {
     try {
-      const officialDocs = await getDocuments(
-        "official-docs",
-        doctype ? { doctype } : null,
-        null,
-        { _id: -1 }
-      );
-      return res.status(200).json(officialDocs);
+      const partners = await getDocuments("partners");
+      return res.status(200).json(partners);
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: "Internal server error" });
@@ -33,33 +27,28 @@ export default async function handler(req, res) {
       // * Validate the data
       try {
         // Define the POST CLUB schema
-        const schema = getSchema("official-docs");
+        const schema = getSchema("partners");
 
         // Actual validation
         validateAPI({ data, schema });
       } catch (error) {
-        console.error("ERROR 400 - officialDocs", error.message);
+        console.error("ERROR 400 - partners", error.message);
         return res.status(400).json({ message: error.message });
       }
 
       // * Send the validated data to the database
       try {
-        // Add the createdAt timestamp
-        data.createdAt = new Date();
-
-        await insertDocument("official-docs", data);
+        await insertDocument("partners", data);
         return res
           .status(201)
           .json({ message: "Le fichier de règle a bien été enregistré." });
       } catch (error) {
-        console.error("ERROR 500, clubs", error.message);
-        return res
-          .status(500)
-          .json({
-            error:
-              "Une erreur est survenue lors de la création du fichier de règles dans la base de données. Veuillez réessayer.",
-            details: error.message,
-          });
+        console.error("ERROR 500, partners", error.message);
+        return res.status(500).json({
+          error:
+            "Une erreur est survenue lors de la création du partenaire dans la base de données. Veuillez réessayer.",
+          details: error.message,
+        });
       }
     }
   }
@@ -67,4 +56,7 @@ export default async function handler(req, res) {
   return res
     .status(401)
     .json({ error: "You must be authorized to create a rule file" });
+
+  // If method is not supported
+  return res.status(405).json({ error: "Method not allowed" });
 }
