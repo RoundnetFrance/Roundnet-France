356d9a3a86894544a3334c64ae134c645971c2a9
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-01-27 14:56:52 +0100

    Added Partners in admin dashboard and menu

--- contents/header/index.js
+++ contents/header/index.js
@@ -1,26 +1,26 @@
 // Menu for front
 const menuElements = [
   {
-    name: 'Compétition',
-    url: '/competition',
-    slug: 'competition',
-    icon: 'emoji_events',
+    name: "Compétition",
+    url: "/competition",
+    slug: "competition",
+    icon: "emoji_events",
     subElements: [
       {
-        name: 'Règles Officielles',
-        url: '/regles',
+        name: "Règles Officielles",
+        url: "/regles",
       },
       // {
       //   name: 'Calendrier',
       //   url: '/competition/calendrier',
       // },
       {
-        name: 'Coupe de France',
-        url: '/competition/coupe-de-france-roundnet',
+        name: "Coupe de France",
+        url: "/competition/coupe-de-france-roundnet",
       },
       {
-        name: 'Rencontres inter-clubs',
-        url: '/competition/rencontres-inter-clubs',
+        name: "Rencontres inter-clubs",
+        url: "/competition/rencontres-inter-clubs",
       },
       // {
       //   name: 'Hall Of Fame',
@@ -30,51 +30,51 @@ const menuElements = [
       //   name: 'Mondiaux 2022',
       //   url: '/competition/mondiaux-2022',
       // }
-    ]
+    ],
   },
   {
-    name: 'Clubs & communautés',
-    url: '/clubs-et-communautes',
-    slug: 'communities',
-    icon: 'supervised_user_circle',
+    name: "Clubs & communautés",
+    url: "/clubs-et-communautes",
+    slug: "communities",
+    icon: "supervised_user_circle",
     subElements: [
       {
-        name: 'Liste des clubs',
-        url: '/clubs-et-communautes/liste-des-clubs',
+        name: "Liste des clubs",
+        url: "/clubs-et-communautes/liste-des-clubs",
       },
       {
-        name: 'Adhérer à la fédération',
-        url: '/clubs-et-communautes/adherer-a-roundnet-france',
+        name: "Adhérer à la fédération",
+        url: "/clubs-et-communautes/adherer-a-roundnet-france",
       },
-    ]
+    ],
   },
   {
-    name: 'Boutique',
-    url: '/boutique',
-    slug: 'shop',
-    icon: 'shopping_cart',
+    name: "Boutique",
+    url: "/boutique",
+    slug: "shop",
+    icon: "shopping_cart",
   },
   {
-    name: 'Qui sommes-nous ?',
-    url: '/qui-sommes-nous',
-    slug: 'about',
-    icon: 'info',
+    name: "Qui sommes-nous ?",
+    url: "/qui-sommes-nous",
+    slug: "about",
+    icon: "info",
     subElements: [
       {
-        name: 'L\'équipe Roundnet France',
-        url: '/qui-sommes-nous/equipe-roundnet-france',
+        name: "L'équipe Roundnet France",
+        url: "/qui-sommes-nous/equipe-roundnet-france",
       },
       {
-        name: 'Statuts officiels',
-        url: '/qui-sommes-nous/statuts-officiels-roundnet-france',
+        name: "Statuts officiels",
+        url: "/qui-sommes-nous/statuts-officiels-roundnet-france",
       },
       // {
       //   name: 'Le projet associatif',
       //   url: '/qui-sommes-nous/projet-associatif',
       // },
       {
-        name: 'Contact',
-        url: '/qui-sommes-nous/contact',
+        name: "Contact",
+        url: "/qui-sommes-nous/contact",
       },
     ],
   },
@@ -83,76 +83,80 @@ const menuElements = [
 // Menu for admin
 const adminElements = [
   {
-    name: 'Dashboard',
-    slug: 'dashboard',
-    icon: 'dashboard',
-    url: '/rf-admin'
+    name: "Dashboard",
+    slug: "dashboard",
+    icon: "dashboard",
+    url: "/rf-admin",
   },
   {
-    name: 'Compétition',
-    slug: 'competition',
-    icon: 'emoji_events',
+    name: "Compétition",
+    slug: "competition",
+    icon: "emoji_events",
     subElements: [
       // {
       //   name: 'Calendrier',
       //   url: '/rf-admin/competition/calendrier',
       // },
       {
-        name: 'Coupe de France',
-        url: '/rf-admin/competition/coupe-de-france',
+        name: "Coupe de France",
+        url: "/rf-admin/competition/coupe-de-france",
       },
       {
-        name: 'Règles',
-        url: '/rf-admin/competition/regles',
+        name: "Règles",
+        url: "/rf-admin/competition/regles",
       },
       {
-        name: 'Rencontres inter-clubs',
-        url: '/rf-admin/competition/ric',
+        name: "Rencontres inter-clubs",
+        url: "/rf-admin/competition/ric",
       },
       // {
       //   name: 'Hall Of Fame',
       //   url: '/rf-admin/competition/hall-of-fame',
       // },
-    ]
-  },
-  {
-    name: 'Clubs & communautés',
-    slug: 'communities',
-    icon: 'supervised_user_circle',
-    subElements: [
-      {
-        name: 'Liste des clubs',
-        url: '/rf-admin/clubs-et-communautes/liste-des-clubs',
-      },
-    ]
-  },
-  {
-    name: 'Association',
-    slug: 'about',
-    icon: 'info',
-    subElements: [
-      {
-        name: 'L\'équipe Roundnet France',
-        url: '/rf-admin/association/equipe-roundnet-france',
-      },
-      {
-        name: 'Statuts',
-        url: '/rf-admin/association/statuts'
-      }
     ],
   },
   {
-    name: 'Paramètres',
-    slug: 'settings',
-    icon: 'settings',
+    name: "Clubs & communautés",
+    slug: "communities",
+    icon: "supervised_user_circle",
     subElements: [
       {
-        name: 'Mon Compte',
-        url: '/rf-admin/parametres/mon-compte',
+        name: "Liste des clubs",
+        url: "/rf-admin/clubs-et-communautes/liste-des-clubs",
+      },
+    ],
+  },
+  {
+    name: "Association",
+    slug: "about",
+    icon: "info",
+    subElements: [
+      {
+        name: "L'équipe Roundnet France",
+        url: "/rf-admin/association/equipe-roundnet-france",
       },
       {
-        name: 'Administrateurs',
-        url: '/rf-admin/parametres/administrateurs',
+        name: "Statuts",
+        url: "/rf-admin/association/statuts",
+      },
+      {
+        name: "Partenaires",
+        url: "/rf-admin/association/partenaires",
+      },
+    ],
+  },
+  {
+    name: "Paramètres",
+    slug: "settings",
+    icon: "settings",
+    subElements: [
+      {
+        name: "Mon Compte",
+        url: "/rf-admin/parametres/mon-compte",
+      },
+      {
+        name: "Administrateurs",
+        url: "/rf-admin/parametres/administrateurs",
       },
     ],
   },
