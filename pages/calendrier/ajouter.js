214751cd96e759ae986c652f532dbb194da4b2ae
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-02-10 13:57:20 +0100

    Added warning content for addingEvent page

--- pages/calendrier/ajouter.js
+++ pages/calendrier/ajouter.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 
 // MUI IMPORTS
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Alert, AlertTitle } from "@mui/material";
 
 // COMPONENT IMPORTS
 import Hero from "../../components/ui/hero";
@@ -24,24 +24,27 @@ export default function CreateEventPage() {
         mini
       />
 
-      <Container maxWidth="md" sx={{ my: 4 }}>
+      <Container maxWidth="md" sx={{ mt: 4 }}>
         <PageTitle title="Donnez de la visibilité à votre tournoi !" />
-        <Typography variant="body1" gutterBottom>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima enim
-          debitis possimus doloribus ipsam modi odio magni repellat a delectus
-          sed iste quis, perferendis excepturi repellendus veritatis. Inventore
-          magnam, fuga adipisci ea aspernatur ipsa facere quidem deleniti,
-          itaque odio doloremque? Voluptatibus eos voluptas neque sit dicta
-          repellat eum.
+        <Typography variant="body1" sx={{ mb: 4 }}>
+          Votre tournoi est fin prêt et vous avez besoin d&apos;un petit coup de
+          pouce dans votre communication ? Ajoutez-le dans le calendrier
+          officiel des tournois et événements de roundnet en France !
         </Typography>
-        <Typography variant="body1" gutterBottom>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima enim
-          debitis possimus doloribus ipsam modi odio magni repellat a delectus
-          sed iste quis, perferendis excepturi repellendus veritatis. Inventore
-          magnam, fuga adipisci ea aspernatur ipsa facere quidem deleniti,
-          itaque odio doloremque? Voluptatibus eos voluptas neque sit dicta
-          repellat eum.
-        </Typography>
+
+        <Alert severity="info" sx={{ my: 2, fontWeight: "bold" }}>
+          Chaque demande de tournoi doit être validée par un membre de la
+          fédération pour apparaître en ligne.
+        </Alert>
+
+        <Alert severity="warning" sx={{ fontWeight: "bold" }}>
+          <AlertTitle sx={{ fontWeight: "bold" }}>Attention !</AlertTitle>
+          Lorsque vous validez votre tournoi, vous ne pourrez plus le modifier
+          par la suite. Soyez bien sûrs des informations que vous entrez ! Dans
+          le cas où certaines informations vous sont encore inconnues (prix,
+          date exacte...), préférez remplir ce formulaire une fois votre
+          événement et ses détails fixés.
+        </Alert>
       </Container>
 
       <CreateEventForm />
