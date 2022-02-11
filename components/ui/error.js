ea6a56dfa9c463cdcb1122aba2805df231a6c453
Niborwood <robin.souriau@gmail.com>
2022-02-11 18:09:55 +0100

    Added proper error handling on getStaticProps eventSlug

--- components/ui/error.js
+++ components/ui/error.js
@@ -2,7 +2,7 @@ import propTypes from "prop-types";
 
 import { Alert } from "@mui/material";
 
-function Error({ message, returnLink }) {
+function Error({ message, sx, returnLink }) {
   return (
     <Alert severity="error" sx={{ my: 2 }}>
       {message}
@@ -13,11 +13,13 @@ function Error({ message, returnLink }) {
 Error.propTypes = {
   message: propTypes.string,
   returnLink: propTypes.string,
+  sx: propTypes.object,
 };
 
 Error.defaultProps = {
   message: "Une erreur est survenue.",
   returnLink: null,
+  sx: {},
 };
 
 export default Error;
--- pages/calendrier/[eventSlug].js
+++ pages/calendrier/[eventSlug].js
@@ -2,25 +2,31 @@ import { getDocuments, getDocument } from "../../helpers/db";
 import { Fragment } from "react";
 
 // MUI IMPORTS
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
 
 // COMPONENTS IMPORTS
 import Head from "../../components/head";
 import Hero from "../../components/ui/hero";
 import EventSingleDetails from "../../components/events/events-single";
 import EventRelated from "../../components/events/events-single/events-related";
+import Error from "../../components/ui/error";
 
-export default function EventSingle({ event, nextEvents }) {
+export default function EventSingle({
+  event,
+  nextEvents,
+  errorEvent,
+  errorNextEvents,
+}) {
   return (
     <Fragment>
       {/* HEAD */}
       <Head
-        title={`${event.title} - Roundnet France`}
-        description={event.description.substring(0, 130) + "..."}
+        title={`${event?.title || "Tournoi de Roundnet"} - Roundnet France`}
+        description={event?.description.substring(0, 130) + "..."}
       />
 
       {/* BODY IMAGE */}
-      <Hero image={event.banner} half />
+      <Hero image={event?.banner || "/images/misc/placeholder.jpg"} half />
 
       {/* PAPER CONTENT */}
       <Container
@@ -31,8 +37,19 @@ export default function EventSingle({ event, nextEvents }) {
           mb: "-20vh",
         }}
       >
-        <EventSingleDetails event={event} />
-        <EventRelated nextEvents={nextEvents} />
+        {errorEvent ? (
+          <Box mt="40vh">
+            <Error message={errorEvent} />
+          </Box>
+        ) : (
+          <EventSingleDetails event={event} />
+        )}
+
+        {errorNextEvents ? (
+          <Error message={errorNextEvents} />
+        ) : (
+          <EventRelated nextEvents={nextEvents} />
+        )}
       </Container>
     </Fragment>
   );
@@ -52,38 +69,52 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const event = await getDocument("events", { slug: params.eventSlug });
+  let errorEvent;
+  let errorNextEvents;
+  let event;
+  let nextEvents;
 
-  // If no event, return 404
-  if (!event) {
-    return {
-      notFound: true,
-    };
+  try {
+    event = await getDocument("events", { slug: params.eventSlug });
+    // If no event, return 404
+    if (!event) {
+      return {
+        notFound: true,
+      };
+    }
+  } catch (err) {
+    errorEvent = err.message;
   }
 
   // Get the next three events by date (from the current event)
-  const nextEvents = await getDocuments(
-    "events",
-    {
-      date: { $gt: new Date().toISOString() },
-      validated: true,
-    },
-    {
-      price: 0,
-      inscriptionUrl: 0,
-      address: 0,
-      organization: 0,
-    },
-    {
-      date: 1,
-    },
-    3
-  );
+  try {
+    nextEvents = await getDocuments(
+      "events",
+      {
+        date: { $gt: new Date().toISOString() },
+        validated: true,
+      },
+      {
+        price: 0,
+        inscriptionUrl: 0,
+        address: 0,
+        organization: 0,
+      },
+      {
+        date: 1,
+      },
+      3
+    );
+  } catch (err) {
+    errorNextEvents = err.message;
+  }
 
   return {
     props: {
-      event: JSON.parse(JSON.stringify(event)),
-      nextEvents,
+      event: event ? JSON.parse(JSON.stringify(event)) : null,
+      nextEvents: nextEvents || null,
+      errorEvent: errorEvent || null,
+      errorNextEvents: errorNextEvents || null,
     },
     revalidate: 600,
   };
