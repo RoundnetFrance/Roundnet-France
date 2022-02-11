09a460517b20d1b20f2fb7fea037eb9e9af3f6aa
Niborwood <robin.souriau@gmail.com>
2022-02-11 17:53:31 +0100

    Added proper error handling on getStaticProps for front pages

--- pages/partenaires.js
+++ pages/partenaires.js
@@ -6,8 +6,9 @@ import { Fragment } from "react";
 import Head from "../components/head";
 import PageTitle from "../components/ui/page-title";
 import CrossingItems from "../components/ui/crossing-items";
+import Error from "../components/ui/error";
 
-export default function PartnersPage({ partners }) {
+export default function PartnersPage({ partners, error }) {
   return (
     <Fragment>
       <Container maxWidth="lg" sx={{ py: 4 }}>
@@ -15,17 +16,29 @@ export default function PartnersPage({ partners }) {
         <PageTitle title="Partenaires officiels" />
       </Container>
       <Container maxWidth="md" sx={{ py: 4 }}>
-        <CrossingItems items={partners} height={300} imageFit="contain" />
+        {error ? (
+          <Error message={error} />
+        ) : (
+          <CrossingItems items={partners} height={300} imageFit="contain" />
+        )}
       </Container>
     </Fragment>
   );
 }
 
 export async function getStaticProps() {
-  const partners = await getDocuments("partners");
+  let partners;
+  let error;
+  try {
+    partners = await getDocuments("partners");
+  } catch (err) {
+    error = err.message;
+  }
+
   return {
     props: {
-      partners,
+      partners: partners || null,
+      error: error || null,
     },
     revalidate: 600,
   };
