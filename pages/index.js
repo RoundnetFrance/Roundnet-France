9f5b0646e21be2133306bb76919c25423b988787
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-04-03 16:35:23 +0200

    Added events on the homepage. Added comments to the sections on the homepage codebase

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import { Fragment } from "react";
 import { getDocuments } from "../helpers/db";
 
 // MUI IMPORTS
-import { Container } from "@mui/material";
+import { Container, Divider, Box } from "@mui/material";
 
 // COMPONENT IMPORTS
+import Link from "../components/ui/link";
 import Hero from "../components/ui/hero";
 import HomeInfoBlock from "../components/home/home-info-block";
 import LogoCarousel from "../components/ui/logo-carousel";
@@ -12,10 +13,12 @@ import FourSquareInfo from "../components/home/four-square-info";
 import CTAFooter from "../components/ui/cta-footer";
 import RulesDemo from "../components/home/rules-demo";
 import Error from "../components/ui/error";
+import EventsTimeline from "../components/events/events-timeline";
 
 export default function HomePage({
   clubLogos,
   partnersLogos,
+  events,
   errorLogos,
   errorPartnersLogos,
 }) {
@@ -31,13 +34,37 @@ export default function HomePage({
         altButtonLink="/clubs-et-communautes/liste-des-clubs"
       />
 
+      {/* Qu'est-ce que Roundnet France ? */}
       <HomeInfoBlock />
+
+      {/* Tournois & événements à venir */}
+      <Container maxWidth="md" sx={{ mb: 12 }}>
+        <EventsTimeline events={events} fullWidth />
+        <Box
+          sx={{ display: "flex", direction: "row", justifyContent: "center" }}
+        >
+          <Link
+            color="secondary"
+            href="/calendrier"
+            isButton
+            buttonIcon="arrow_forward"
+          >
+            Voir les autres tournois
+          </Link>
+        </Box>
+      </Container>
+
+      <Divider />
+
+      {/* Les démarches de la fédération */}
       <FourSquareInfo />
 
+      {/* Les règles du roundnet... */}
       <div id="rules-demo">
         <RulesDemo />
       </div>
 
+      {/* CTA before footer */}
       <CTAFooter
         title="Motivé.e ? Envie de jouer ou de participer ?"
         subtitle="Nous sommes là pour vous accompagner !"
@@ -51,6 +78,7 @@ export default function HomePage({
         }}
       />
 
+      {/* Ils adhèrent à Roundnet France */}
       {errorLogos ? (
         <Container maxWidth="sm">
           <Error message="Une erreur est survenue lors de la récupération des logos des clubs" />
@@ -62,6 +90,7 @@ export default function HomePage({
         />
       )}
 
+      {/* Nos partenaires */}
       {errorPartnersLogos ? (
         <Container maxWidth="sm">
           <Error message="Une erreur est survenue lors de la récupération des logos des partenaires" />
@@ -76,6 +105,7 @@ export default function HomePage({
 export async function getStaticProps() {
   let errorLogos;
   let clubLogos;
+  let events;
   let errorPartnersLogos;
   let partnersLogos;
 
@@ -103,6 +133,7 @@ export async function getStaticProps() {
     errorLogos = err.message;
   }
 
+  // Get partners
   try {
     const partners = await getDocuments("partners", null, {
       image: 1,
@@ -118,10 +149,34 @@ export async function getStaticProps() {
     errorPartnersLogos = err.message;
   }
 
+  // Get events
+  try {
+    events = await getDocuments(
+      "events",
+      { validated: true, date: { $gte: new Date().toJSON() } },
+      {
+        price: 0,
+        inscriptionUrl: 0,
+        address: 0,
+        organization: 0,
+      },
+      { date: 1 },
+      3
+    );
+    partnersLogos = partners.map((partner) => ({
+      src: partner.image,
+      alt: partner.title,
+      link: partner.links,
+    }));
+  } catch (err) {
+    errorPartnersLogos = err.message;
+  }
+
   return {
     props: {
       clubLogos: clubLogos || null,
       partnersLogos: partnersLogos || null,
+      events: events || null,
       errorLogos: errorLogos || null,
       errorPartnersLogos: errorPartnersLogos || null,
     },
