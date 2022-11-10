6887a5409debb56ca4e8095c685c6d0cbb23e842
Robin SOURIAU <robin.souriau@gmail.com>
2022-11-10 21:43:22 +0100

    Add a bunch of trycatch for api sessions

--- pages/api/clubs/admin/index.js
+++ pages/api/clubs/admin/index.js
@@ -2,26 +2,31 @@ import { getSession } from "next-auth/react";
 import { getDocuments } from "../../../../helpers/db";
 
 export default async function handler(req, res) {
-  const session = await getSession({ req });
+  try {
+    const session = await getSession({ req });
 
-  // If user is authorized
-  if (session) {
-
-    // GET method to read federation members (including non-validated ones)
-    if (req.method === 'GET') {
-      try {
-        const clubs = await getDocuments('clubs');
-        return res.status(200).json(clubs);
-      } catch (error) {
-        return res.status(500).json({ error: 'Internal server error', details: error.message });
+    // If user is authorized
+    if (session) {
+      // GET method to read federation members (including non-validated ones)
+      if (req.method === "GET") {
+        try {
+          const clubs = await getDocuments("clubs");
+          return res.status(200).json(clubs);
+        } catch (error) {
+          return res
+            .status(500)
+            .json({ error: "Internal server error", details: error.message });
+        }
       }
+
+      return res.status(405).json({ error: "Method not allowed" });
     }
 
-    return res.status(405).json({ error: 'Method not allowed' });
+    // If user is not authorized
+    res.send({
+      error: "You must be sign in to view the protected content on this page.",
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-
-  // If user is not authorized
-  res.send({
-    error: "You must be sign in to view the protected content on this page.",
-  });
 }
--- pages/api/official-docs/index.js
+++ pages/api/official-docs/index.js
@@ -4,67 +4,69 @@ import { validateAPI } from "../../../helpers/form";
 import getSchema from "../../../helpers/schemas";
 
 export default async function handler(req, res) {
-  // Get the doctype. Will be used to only fetch docs of that type
-  const { doctype } = req.query;
+  try {
+    // Get the doctype. Will be used to only fetch docs of that type
+    const { doctype } = req.query;
 
-  const session = await getSession({ req });
-  // GET method to read rule files
-  if (req.method === "GET") {
-    try {
-      const officialDocs = await getDocuments(
-        "official-docs",
-        doctype ? { doctype } : null,
-        null,
-        { _id: -1 }
-      );
-      return res.status(200).json(officialDocs);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: "Internal server error" });
-    }
-  }
-
-  // Protected by session
-  if (session) {
-    // POST method to create a new rule file (only for admins)
-    if (req.method === "POST") {
-      const { data } = req.body;
-
-      // * Validate the data
+    const session = await getSession({ req });
+    // GET method to read rule files
+    if (req.method === "GET") {
       try {
-        // Define the POST CLUB schema
-        const schema = getSchema("official-docs");
-
-        // Actual validation
-        validateAPI({ data, schema });
+        const officialDocs = await getDocuments(
+          "official-docs",
+          doctype ? { doctype } : null,
+          null,
+          { _id: -1 }
+        );
+        return res.status(200).json(officialDocs);
       } catch (error) {
-        console.error("ERROR 400 - officialDocs", error.message);
-        return res.status(400).json({ message: error.message });
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error" });
       }
+    }
 
-      // * Send the validated data to the database
-      try {
-        // Add the createdAt timestamp
-        data.createdAt = new Date();
+    // Protected by session
+    if (session) {
+      // POST method to create a new rule file (only for admins)
+      if (req.method === "POST") {
+        const { data } = req.body;
 
-        await insertDocument("official-docs", data);
-        return res
-          .status(201)
-          .json({ message: "Le fichier de règle a bien été enregistré." });
-      } catch (error) {
-        console.error("ERROR 500, clubs", error.message);
-        return res
-          .status(500)
-          .json({
+        // * Validate the data
+        try {
+          // Define the POST CLUB schema
+          const schema = getSchema("official-docs");
+
+          // Actual validation
+          validateAPI({ data, schema });
+        } catch (error) {
+          console.error("ERROR 400 - officialDocs", error.message);
+          return res.status(400).json({ message: error.message });
+        }
+
+        // * Send the validated data to the database
+        try {
+          // Add the createdAt timestamp
+          data.createdAt = new Date();
+
+          await insertDocument("official-docs", data);
+          return res
+            .status(201)
+            .json({ message: "Le fichier de règle a bien été enregistré." });
+        } catch (error) {
+          console.error("ERROR 500, clubs", error.message);
+          return res.status(500).json({
             error:
               "Une erreur est survenue lors de la création du fichier de règles dans la base de données. Veuillez réessayer.",
             details: error.message,
           });
+        }
       }
     }
-  }
 
-  return res
-    .status(401)
-    .json({ error: "You must be authorized to create a rule file" });
+    return res
+      .status(401)
+      .json({ error: "You must be authorized to create a rule file" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
