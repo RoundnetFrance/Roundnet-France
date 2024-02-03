58c4d674e419bdfa78f7dbcf552c22a1dbba7e3f
romainchaulet <51263552+romainchaulet@users.noreply.github.com>
2024-02-03 21:38:24 +0100

    Boutique : change url & image
    boutique page : change main image and add url to helloAsso RF

--- contents/boutique/index.js
+++ contents/boutique/index.js
@@ -1,32 +1,12 @@
 const shopContent = [
   {
     _id: "1",
-    title: "Roundnet France possède des liens privilégiés avec Urban Roundnet",
+    title: "Boutique Roundnet France",
+    image: "/images/pages/boutique/boutique-rf.jpg",
     description:
-      "Urban Roundnet est la première salle dédiée intégralement au roundnet en France ! En plus d’accueillir des joueurs toute l’année sur Paris, Urban Roundnet proposera bientôt la vente de sets Spikeball en livraison sur toute la France !",
-    links: "https://www.urbanroundnet.com/",
-  },
-  {
-    _id: "2",
-    title: "...ou passez par votre association locale !",
-    image: "/images/pages/boutique/association-boutique.jpg",
-    description:
-      "Vous pouvez passer par votre association locale afin d'obtenir un set. Les conditions d'envoi sont variables en fonction des clubs.",
-    links: [
-      {
-        url: "https://titansroundnet.fr/boutique/",
-        text: "Nantes",
-      },
-      {
-        url: "https://www.facebook.com/roundnet.paris/",
-        text: "Paris",
-      },
-      {
-        url: "https://www.facebook.com/RoundnetToulouse",
-        text: "Toulouse",
-      },
-    ],
-  },
+      "Emparez vous des articles de la boutique Roundnet France pour parfaire votre collection ",
+    links: "https://www.helloasso.com/associations/roundnet-france/boutiques/boutique-roundnet-france",
+  }
 ];
 
 export { shopContent };
