aa28c83eecf7fe9b791baf9d2afe117106b7e5f4
Robin Souriau <rsouriau@lacentrale.fr>
2024-05-07 20:16:58 +0200

    Fix date in forms

--- pages/calendrier/ajouter.tsx
+++ pages/calendrier/ajouter.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment } from "react";
+import { type FC, Fragment } from "react";
 
 import { Container, Typography, Alert, AlertTitle } from "@mui/material";
 
@@ -11,37 +11,37 @@ const CreateEventPage: FC = () => {
   return (
     <Fragment>
       <Head
-        title="Ajouter un évent ou tournoi de roundnet - Roundnet France"
-        description="Profitez de la visibilité de Roundnet France pour ajouter un évent ou un tournoi de roundnet le faire connaître !"
+        title='Ajouter un évent ou tournoi de roundnet - Roundnet France'
+        description='Profitez de la visibilité de Roundnet France pour ajouter un évent ou un tournoi de roundnet le faire connaître !'
       />
 
       <Hero
-        title="Ajouter un tournoi"
-        image="/images/hero/liste-clubs.jpg"
-        imagePosition="center 60%"
+        title='Ajouter un tournoi'
+        image='/images/hero/liste-clubs.jpg'
+        imagePosition='center 60%'
         mini
       />
 
-      <Container maxWidth="md" sx={{ mt: 4 }}>
-        <PageTitle title="Donnez de la visibilité à votre tournoi !" />
-        <Typography variant="body1" sx={{ mb: 4 }}>
+      <Container maxWidth='md' sx={{ mt: 4 }}>
+        <PageTitle title='Donnez de la visibilité à votre tournoi !' />
+        <Typography variant='body1' sx={{ mb: 4 }}>
           Votre tournoi est fin prêt et vous avez besoin d&apos;un petit coup de
           pouce dans votre communication ? Ajoutez-le dans le calendrier
           officiel des tournois et événements de roundnet en France !
         </Typography>
 
-        <Alert severity="info" sx={{ my: 2, fontWeight: "bold" }}>
+        <Alert severity='info' sx={{ my: 2, fontWeight: "bold" }}>
           Chaque demande de tournoi doit être validée par un membre de la
           fédération pour apparaître en ligne.
         </Alert>
 
-        <Alert severity="warning" sx={{ fontWeight: "bold" }}>
+        <Alert severity='warning' sx={{ fontWeight: "bold" }}>
           <AlertTitle sx={{ fontWeight: "bold" }}>Attention !</AlertTitle>
           Lorsque vous validez votre tournoi, vous ne pourrez plus le modifier
           par la suite. Essayez d&apos;avoir un maximum d&apos;informations
           avant de le remplir ! Si vous souhaitez modifier ou compléter votre
           événement à posteriori, vous pouvez contacter la fédération via le{" "}
-          <Link href="/qui-sommes-nous/contact">formulaire de contact</Link>.
+          <Link href='/qui-sommes-nous/contact'>formulaire de contact</Link>.
         </Alert>
       </Container>
 
